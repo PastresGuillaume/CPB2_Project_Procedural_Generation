72925880ea0cab40fc64cd391319f1dbf3010a1a
Pastres Guillaume <pastres.guillaume@gmail.com>
2021-03-09 17:29:22 +0100

    Génération à partir de seed aléatoires

--- interface.py
+++ interface.py
@@ -1,7 +1,16 @@
 import tkinter as tk
 from main import Map
 
-map_test = Map()
+
+def tk_propagate_method():
+    data = {
+        "width" : int(im_width.get()),
+        "height" : int(im_height.get()),
+        "seed" : int(seed.get()),
+        "seed_range": int(scale_seed_range.get())
+    }
+    map_propagate_method = Map(data)
+    map_propagate_method.start()
 
 
 window = tk.Tk()
@@ -14,8 +23,33 @@ canvas.pack()
 propagate_label = tk.Label(window, text="Génération par propagation")
 propagate_label.pack()
 
-button1 = tk.Button(window, text="Start", command=map_test.start)
-button1.pack()
+# Interface, paramètres méthode par propagations
+
+# Definition de la taille de l'image
+label_im_width = tk.Label(window, text="Width")
+im_width = tk.Spinbox(window, from_=100, to=1000, increment =1)
+label_im_width.pack()
+im_width.pack()
+
+label_im_height = tk.Label(window, text="Height")
+im_height = tk.Spinbox(window, from_=100, to=900, increment =1)
+label_im_height.pack()
+im_height.pack()
+
+# Nombre de node au départ
+label_seed = tk.Label(window, text="Seed Number")
+seed = tk.Spinbox(window, from_=1, to=1000, increment =1)
+label_seed.pack()
+seed.pack()
+
+# Range des Seeds
+scale_seed_range=tk.Scale(window, orient='horizontal', from_=1, to=9,resolution=1, length=300,label='Seed Range')
+scale_seed_range.set(5)
+scale_seed_range.pack()
+
+
+button_propagate = tk.Button(window, text="Start", command=tk_propagate_method)
+button_propagate.pack()
 
 
 window.mainloop()
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from random import randint
+from random import randint, choice
 from PIL import Image
 
 
 max_height = 200
 min_height = -100
+empty_node = None
+
 
 color_panel = {
     (-100, -75): (0, 15, 198),
@@ -25,15 +27,18 @@ color_panel = {
 
 class Case:
     def __init__(self):
-        self.height = 0
+        self.height = empty_node
 
 
 class Map:
-    def __init__(self):
-        self.width = 500
-        self.height = 500
+    def __init__(self, data):
+        self.width = data["width"]
+        self.height = data["height"]
+        self.seed = data["seed"]
+        self.seed_range = data["seed_range"]
         self.map = []
         self.img_map = Image.new("RGB", (self.width, self.height), 0)
+        self.starting_node = []
 
     def show(self):
         for y in range(self.height):
@@ -46,38 +51,45 @@ class Map:
             self.map.append([])
             for x in range(self.width):
                 self.map[y].append(Case())
-        self.map[0][0].height = randint(min_height, max_height)
+
+    def node_spawn(self):
+        while len(self.starting_node) < self.seed:
+            x = randint(0, self.width-1)
+            y = randint(0, self.height-1)
+            if self.map[y][x].height == empty_node:
+                self.map[y][x].height = randint(min_height, max_height)
+                self.starting_node.append((y, x))
 
     def propagate(self):
+        while len(self.starting_node) !=0:
+            node = choice(self.starting_node)
+            near_nodes = []
+            for ybis in range(node[0] - self.seed_range, node[0] + self.seed_range + 1):
+                for xbis in range(node[1] - self.seed_range, node[1] + self.seed_range + 1):
+                    if 0 <= ybis < self.height and 0 <= xbis < self.width and not (ybis == node[0] and xbis == node[1]):
+                        if self.map[ybis][xbis].height == empty_node:
+                            near_nodes.append((ybis, xbis))
+            while len(near_nodes) !=0:
+                new_node = choice(near_nodes)
+                self.map[new_node[0]][new_node[1]].height = self.map[node[0]][node[1]].height
+                near_nodes.remove(new_node)
+            self.starting_node.remove(node)
+
+    def convert_image(self):
         for y in range(self.height):
             for x in range(self.width):
-                if 0 < y < self.height-1 and 0 < x < self.width-1:
-                    square = self.map[y-1][x-1].height + self.map[y-1][x].height + self.map[y][x-1].height
-                    if round(square/15) < 1:
-                        al = randint(-1, 1)
-                    else:
-                        al = randint(round(-square/15), round(square/15))
-                    self.map[y][x].height = round(square/3 + al)
-                    if self.map[y][x].height > max_height:
-                        self.map[y][x].height = max_height
-                    elif self.map[y][x].height < min_height:
-                        self.map[y][x].height = min_height
+                if self.map[y][x].height == empty_node:
+                    self.img_map.putpixel((x, y), (0, 0, 0))
                 else:
-                    if x != 0 and (y == 0 or y == self.height):
-                        self.map[y][x].height = self.map[y][x-1].height + round(1/5*randint(min_height, max_height))
-                        if self.map[y][x].height > max_height:
-                            self.map[y][x].height = max_height
-                        elif self.map[y][x].height < min_height:
-                            self.map[y][x].height = min_height
-                    else:
-                        self.map[y][x].height = self.map[y-1][x].height
-                for color in color_panel.keys():
-                    if color[0] < self.map[y][x].height <= color[1]:
-                        self.img_map.putpixel((x,y), color_panel[color])
+                    for color in color_panel.keys():
+                        if color[0] < self.map[y][x].height <= color[1]:
+                            self.img_map.putpixel((x,y), color_panel[color])
         self.img_map.show()
 
     def start(self):
         self.init_map()
+        self.node_spawn()
         self.propagate()
+        self.convert_image()
 
 
